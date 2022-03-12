63d8fc3bf880c0c4e2dbbd97a230c1c6b8a2f42d
muammarahlnn <21muammarahlan@gmail.com>
2022-03-12 12:35:44 +0800

    add OkHttpClient

--- app/src/main/java/com/ardnn/flix/core/data/source/remote/ApiConfig.kt
+++ app/src/main/java/com/ardnn/flix/core/data/source/remote/ApiConfig.kt
@@ -4,8 +4,11 @@ import com.ardnn.flix.BuildConfig
 import com.ardnn.flix.core.data.source.remote.service.MovieApiService
 import com.ardnn.flix.core.data.source.remote.service.PersonApiService
 import com.ardnn.flix.core.data.source.remote.service.TvShowApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiConfig {
     const val API_KEY = BuildConfig.API_KEY
@@ -24,6 +27,16 @@ object ApiConfig {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttpClient())
+            .build()
+    }
+
+    private fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
             .build()
     }
 }
